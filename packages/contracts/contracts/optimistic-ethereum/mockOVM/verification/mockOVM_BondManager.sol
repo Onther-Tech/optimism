90da1e8a31a9e044b52d66ad3681102c03a79fd3
modagi <modagi@gmail.com>
2021-05-18 03:57:43 +0000

    customizing init

--- packages/contracts/contracts/optimistic-ethereum/mockOVM/verification/mockOVM_BondManager.sol
+++ packages/contracts/contracts/optimistic-ethereum/mockOVM/verification/mockOVM_BondManager.sol
@@ -3,19 +3,32 @@ pragma solidity >0.5.0 <0.8.0;
 
 /* Interface Imports */
 import { iOVM_BondManager } from "../../iOVM/verification/iOVM_BondManager.sol";
+import { TokamakConnector } from "../../tokamak-network/TokamakConnector.sol";
 
 /* Contract Imports */
 import { Lib_AddressResolver } from "../../libraries/resolver/Lib_AddressResolver.sol";
 
+//import { ISeigManager } from "../../tokamak-network/interfaces/ISeigManager.sol";
+
 /**
  * @title mockOVM_BondManager
  */
-contract mockOVM_BondManager is iOVM_BondManager, Lib_AddressResolver {
+contract mockOVM_BondManager is TokamakConnector, iOVM_BondManager, Lib_AddressResolver {
+    //ISeigManager public seigManager;
+
     constructor(
-        address _libAddressManager
+        address _libAddressManager,
+        address _seigManager
     )
         Lib_AddressResolver(_libAddressManager)
-    {}
+        TokamakConnector(_seigManager)
+    {
+        //seigManager = ISeigManager(_seigManager);
+    }
+
+    /*function setSeigManager(address _seigManager) override external {
+        seigManager = ISeigManager(_seigManager);
+    }*/
 
     function recordGasSpent(
         bytes32 _preStateRoot,
@@ -69,7 +82,9 @@ contract mockOVM_BondManager is iOVM_BondManager, Lib_AddressResolver {
         )
     {
         // Only authenticate sequencer to submit state root batches.
-        return _who == resolve("OVM_Proposer");
+        //return _who == resolve("OVM_Proposer");
+
+        return stakedMinimumAmount(_who);
     }
 
     function getGasSpent(
