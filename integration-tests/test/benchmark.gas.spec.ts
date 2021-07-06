88d115feff4aa6634337e5298b97bc19c72d02bc
modagi <modagi@gmail.com>
2021-07-06 08:48:07 +0000

    integration_tests: edit gas test

--- integration-tests/test/benchmark.gas.spec.ts
+++ integration-tests/test/benchmark.gas.spec.ts
@@ -7,12 +7,13 @@ import { OptimismEnv } from './shared/env'
 import { solidity } from 'ethereum-waffle'
 import { Direction } from './shared/watcher-utils'
 
-import CONSUMER_L1_JSON from '../artifacts/contracts/GasConsumer.sol/GasConsumer.json'
-import CONSUMER_L2_JSON from '../artifacts-ovm/contracts/GasConsumer.sol/GasConsumer.json'
+import GasConsumer_L1_JSON from '../artifacts/contracts/GasConsumer.sol/GasConsumer.json'
+import GasConsumer_L2_JSON from '../artifacts-ovm/contracts/GasConsumer.sol/GasConsumer.json'
 
 chai.use(solidity)
 
 const DEFAULT_TEST_GAS_L1 = 330_000
+const DEFAULT_TEST_GAS_L2 = 1_300_000
 
 describe('Basic GasConsumer interactions', async () => {
   const initialAmount = 1000000
@@ -25,87 +26,154 @@ describe('Basic GasConsumer interactions', async () => {
   let other: Wallet
   let Factory__GasConsumer_L1: ContractFactory
   let Factory__GasConsumer_L2: ContractFactory
-  let GasConsumer_L2: Contract
-  let GasConsumer_L1: Contract
+  let gasConsumer_L2: Contract
+  let gasConsumer_L1: Contract
 
   let startTime: number
   let startBlockl1: number
   let startBlockl2: number
   let users: Wallet[] = []
 
+  const isL1 = false
+  const gasLimit = 1_000_000
+  const txsCount = 100
+
+  const prepareL1Env = async (env) => {
+    Factory__GasConsumer_L1 = new ContractFactory(
+      GasConsumer_L1_JSON.abi,
+      GasConsumer_L1_JSON.bytecode,
+      wallet_l1
+    )
+    gasConsumer_L1 = await Factory__GasConsumer_L1.deploy()
+    await gasConsumer_L1.deployTransaction.wait()
+  }
+
+  const prepareL2Env = async (env) => {
+    Factory__GasConsumer_L2 = new ContractFactory(
+      GasConsumer_L2_JSON.abi,
+      GasConsumer_L2_JSON.bytecode,
+      wallet_l2
+    )
+    
+    gasConsumer_L2 = await Factory__GasConsumer_L2.deploy()
+    await gasConsumer_L2.deployTransaction.wait()
+  }
+
+  const prepareEnv = async (env) => {
+    if (isL1) {
+      await prepareL1Env(env)
+    } else {
+      await prepareL2Env(env)
+    }
+
+    for (let i = 0; i < txsCount; i++) {
+      const provider = isL1 ? l1Provider : l2Provider
+      let newWallet = Wallet.createRandom().connect(provider)
+      users.push(newWallet)
+
+      if (isL1) {
+        await (await wallet_l1.sendTransaction({
+          to: newWallet.address,
+          value: ethers.utils.parseEther("1.0")
+        })).wait()
+      } else {
+        await env.waitForXDomainTransaction(
+          env.l1Bridge.depositETHTo(newWallet.address, DEFAULT_TEST_GAS_L2, '0xFFFF', {
+            value: ethers.utils.parseEther("10"),
+            gasLimit: DEFAULT_TEST_GAS_L1
+          }),
+          Direction.L1ToL2
+        )
+      }
+    }
+  }
+
   before(async () => {
     const env = await OptimismEnv.new()
     wallet_l2 = env.l2Wallet
     wallet_l1 = env.l1Wallet
     other = Wallet.createRandom().connect(ethers.provider)
-    Factory__GasConsumer_L1 = new ContractFactory(
-      CONSUMER_L1_JSON.abi,
-      CONSUMER_L1_JSON.bytecode,
-      wallet_l1
-    )
-    Factory__GasConsumer_L2 = new ContractFactory(
-      CONSUMER_L2_JSON.abi,
-      CONSUMER_L2_JSON.bytecode,
-      wallet_l2
-    )
-    
-    GasConsumer_L2 = await Factory__GasConsumer_L2.deploy()
-    await GasConsumer_L2.deployTransaction.wait()
-    GasConsumer_L1 = await Factory__GasConsumer_L1.deploy()
-    await GasConsumer_L1.deployTransaction.wait()
 
-    for (let i = 0; i < 300; i++) {
-      let newWallet = Wallet.createRandom().connect(l1Provider)
-      users.push(newWallet)
-
-      await (await wallet_l1.sendTransaction({
-        to: newWallet.address,
-        value: ethers.utils.parseEther("1.0")
-      })).wait()
-
-      await env.ovmEth.transfer(newWallet.address, ethers.utils.parseEther("1.0"))
-    }
+    await prepareEnv(env)
 
     await l1Provider.send("evm_setAutomine", [false])
     await l1Provider.send("evm_setIntervalMining", [1000])
   })
 
+  let a
   beforeEach(async () => {
     startTime = Date.now()
-    startBlockl1 = await l1Provider.getBlockNumber()
-    startBlockl2 = await l2Provider.getBlockNumber()
+    const provider = isL1 ? l1Provider : l2Provider
+    const startBlock = await provider.getBlockNumber()
+    console.log(`startBlock: ${startBlock}`)
+    a = startBlock
   })
 
   afterEach(async () => {
     const duration = Date.now() - startTime
     console.log(`duration: ${duration}`)
 
-    const endBlockl1 = await l1Provider.getBlockNumber()
-    const endBlockl2 = await l2Provider.getBlockNumber()
+    const provider = isL1 ? l1Provider : l2Provider
+    const endBlock = await provider.getBlockNumber()
+    console.log(`endBlock: ${endBlock}`)
 
-    let txCount = 0
-    for (let i = startBlockl1 + 1; i < endBlockl1; i++) {
+    /*for (let i = a+1; i <= endBlock; i++) {
       const block = await l1Provider.getBlock(i)
-      txCount += block.transactions.length
-    }
+      console.log(`block: ${JSON.stringify(block)}`)
+    }*/
   })
 
-  const sendTx = async (GasConsumer: Contract, other: Wallet) => {
-    const gasLimit = 1_000_000
-
-    const txs = users.map(async (user: Wallet) => {
-      (await GasConsumer.connect(user).consume({
-        gasLimit: gasLimit
-      })).wait()
+  const sendTxs = async (gasConsumer: Contract, other: Wallet) => {
+    /*const testTx = await gasConsumer.connect(users[0]).consume({
+      gasLimit: gasLimit
     })
-    await Promise.all(txs)
+    await testTx.wait()
+    const receipt = await l1Provider.getTransactionReceipt(testTx.hash)
+    console.log(`receipt: ${JSON.stringify(receipt)}`)*/
+
+    /*let txs;
+    if (isL1) {
+      txs = users.map(async (user: Wallet) => {
+        (await gasConsumer.connect(user).consume({
+          gasLimit: gasLimit
+        })).wait()
+      })
+    } else {
+      txs = users.map(async (user: Wallet) => {
+        (await gasConsumer.connect(user).consume({
+          gasLimit: 6380000
+        })).wait()
+      })
+    }*/
+
+    //console.log(`txs length: ${txs.length}`)
+    //await Promise.all(txs)
+
+    const testGasLimit = isL1 ? gasLimit : 6380000
+    let txs = []
+    for (let i = 0; i < users.length; i++) {
+      txs.push(gasConsumer.connect(users[i]).consume({
+        gasLimit: testGasLimit
+      }))
+    }
+
+    let txs2 = []
+    for (let i = 0; i < users.length; i++) {
+      txs2.push(await txs[i])
+    }
+
+    for (let i = 0; i < users.length; i++) {
+      await txs2[i].wait()
+    }
   }
 
-  it('Sequentially L1', async () => {
-    await sendTx(GasConsumer_L1, other)
-  })
-
-  /*it('Sequentially L2', async () => {
-    await sendTx(GasConsumer_L2, other)
-  })*/
+  if (isL1) {
+    it('Sequentially L1', async () => {
+      await sendTxs(gasConsumer_L1, other)
+    })
+  } else {
+    it('Sequentially L2', async () => {
+      await sendTxs(gasConsumer_L2, other)
+    })
+  }
 })
